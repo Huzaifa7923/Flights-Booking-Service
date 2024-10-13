7762b5366cab19498fb1c84dd75258e3fc5fb821
Huzaifa <huzaifa.ansari.7923@gmail.com>
2024-10-13 17:09:12 +0530

    added idempotancy key for payment

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -3,6 +3,9 @@ const {SuccessResponse,ErrorResponse} = require('../utils/common')
 
 const {BookingService}=require('../services');
 const { StatusCodes } = require('http-status-codes');
+const AppError = require('../utils/errors/app-error');
+
+const inMemDbForIdempotency={};
 
 const createBooking=async(req,res)=>{
     try{
@@ -29,12 +32,25 @@ const createBooking=async(req,res)=>{
 const makePayment=async(req,res)=>{
     try{
         // console.log('make payment');
+
+        const idempotencyKey=req.header['idempotency-key'];
+
+        if(!idempotencyKey){
+            throw new AppError('idempotency key missing',StatusCodes.BAD_REQUEST);
+        }
+
+        if(inMemDbForIdempotency[idempotencyKey]){
+            SuccessResponse.data={message:'Payment for this transaction already done!'}
+            return res.status(StatusCodes.BAD_REQUEST).json(SuccessResponse);
+        }
+        
         const response=await BookingService.makePayment({
             userId:req.body.userId,
             bookingId:req.body.bookingId,
             totalCost:req.body.totalCost
         })
         SuccessResponse.data=response;
+        inMemDbForIdempotency[idempotencyKey] = idempotencyKey;
         return res.status(StatusCodes.CREATED).json(SuccessResponse)
 
     }catch(err){
